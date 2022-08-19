97a1af938dfa8e120cb7968850f973d99de1dd85
vasanth <vasanth@drylogics.com>
2022-08-19 16:19:09 +0530

    added test for binary search

--- src/binary_search.rb
+++ src/binary_search.rb
@@ -1,20 +1,44 @@
-# arr = [5, 7, 11, 13, 15, 16, 21, 23, 25, 30]
 def binary_search(arr, n)
-    low = 0
-    high = arr.length - 1
-     while low < high
+  low = 0
+  high = arr.length - 1
+   while low < high
+     mid = high - low +1 / 2 +low
+     if  arr[mid] == n
+       return true
+     elsif  arr[mid] < n
+       low = low + 1
        mid = high - low +1 / 2 +low
-       if  arr[mid] == n
-         return true
-       elsif  arr[mid] < n
-         low = low + 1
-         mid = high - low +1 / 2 +low
-       else
-         high = high - 1
-         mid = high - low +1 / 2 +low
-       end
+     else
+       high = high - 1
+       mid = high - low +1 / 2 +low
      end
-     false
+   end
+   false
+end
+
+
+arr = [5, 7, 11, 13, 15, 16, 21, 23, 25, 30]
+
+
+arr.each_with_index do |n, i|
+  if(i != binary_search(arr, n))
+    raise "Error: Failed to correctly find #{n}"
   end
-  puts binary_search(arr, 35)
-  
+end
+
+# out of range
+[4, 32].each do |n|
+  if(-1 != binary_search(arr, n))
+    raise "Error: Did not return -1 for number #{n}"
+  end
+end
+
+
+# In Range, non-existent
+[17, 8, 22, 27].each do |n|
+  if(-1 != binary_search(arr, n))
+    raise "Error: Did not return -1 for number #{n}"
+  end
+end
+
+puts "Binary Search Works!!!"
