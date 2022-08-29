be3430cc8d2d6c7a090dec254b9da9df2b578abd
kowsi96 <kowsalya@drylogics.com>
2022-08-29 20:33:20 +0530

    upto binary_search

--- src/class_util_process.rb
+++ src/class_util_process.rb
@@ -27,9 +27,16 @@ class Util
         }
         result[0]
     end
+
+    def self.find(arr)
+        result = process(arr){|result,e|
+          result << e if yield(e)        
+        }
+        result[0]
+    end
 end
   
-  class Students
+class Students
     attr_reader :id, :college, :branch, :section, :roll_no, :name, :aadhar_no, :pan_no, :pincode
   
     def initialize(id, college, branch, section, roll_no, name, aadhar_no, pan_no, pincode)
@@ -43,23 +50,29 @@ end
       @pan_no = pan_no
       @pincode = pincode
     end
-  end 
+end 
   
-  ann = Students.new(19106001, "arcas", "bsc_cs", "section_A", 01, "Ann", 112536654858, "BHVHG4851J", 654425)
-  robin = Students.new(19106002, "arcas", "bsc_cs", "section_A", 02, "robin", 458522664488, "BSDEF4851J", 652215)
-  cindy = Students.new(19106003, "arcas", "bsc_cs", "section_A", 03, "cindy", 552536654858, "BHVNN4851J", 633218)
-  john = Students.new(19106004, "arcas", "bsc_cs", "section_A", 04, "john", 872536654858, "BSDFC4851J", 659415)
+ann = Students.new(19106001, "arcas", "bsc_cs", "section_A", 01, "Ann", 112536654858, "BHVHG4851J", 654425)
+robin = Students.new(19106002, "arcas", "bsc_cs", "section_A", 02, "robin", 458522664488, "BSDEF4851J", 652215)
+cindy = Students.new(19106003, "arcas", "bsc_cs", "section_A", 03, "cindy", 552536654858, "BHVNN4851J", 633218)
+john = Students.new(19106004, "arcas", "bsc_cs", "section_A", 04, "john", 872536654858, "BSDFC4851J", 659415)
   
-  arr = [ann, robin, cindy, john]
-  result = Util.map(arr){|i| i.name}
-  print result
+arr = [ann, robin, cindy, john]
+result = Util.map(arr){|i| i.name}
+print result
   
-  #select
-  arr = 20.times.to_a
-  result = Util.select(arr){|i| i % 2 == 0 }
-  print result
-  
-  #max_by
-  arr = 20.times.to_a
-  res = Util.max_by(arr){|i| i}
-  puts res
+#select
+arr = 20.times.to_a
+result = Util.select(arr){|i| i % 2 == 0 }
+print result
+
+#max_by
+arr = 20.times.to_a
+res = Util.max_by(arr){|i| i}
+puts res
+
+#find
+arr = 20.times.to_a
+number = 4
+res = Util.find(arr){|i| i == number}
+puts res
